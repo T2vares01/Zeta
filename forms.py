95e4043ae912e2f51dac4d00910e7711bfdce8c9
T2vares01 <134638696+T2vares01@users.noreply.github.com>
2024-07-30 18:57:01 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField ,FileField
 from wtforms.validators import DataRequired,Email,EqualTo,Length,ValidationError
 from Site.models import Usuario
 
@@ -17,7 +17,11 @@ class FormCriarConta(FlaskForm):
     botao = SubmitField("Criar conta")
 
     def validate_email(self, email):
-        usuario = Usuario.query.filter_by(email = email.data).first()
+        usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            return ValidationError("Email ja cadastrado")
+            return ValidationError("E-mail já cadastrado, faça login para continuar")
+            
+class FormFoto(FlaskForm):
+    foto = FileField("Foto", validators=[DataRequired()])
+    botao = SubmitField("Enviar")
 
