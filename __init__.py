4a246970d74c3568679f41d55ae9ab9f17e69889
T2vares01 <134638696+T2vares01@users.noreply.github.com>
2024-07-30 18:56:30 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template as rt, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "50a9156b31c864267d3a13571672c5104cd27288baa95f61c5e702345e4c14e9"
+app.config["UPLOAD_FOLDER"] = "static/fotos_post"
+io = SocketIO(app)
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
